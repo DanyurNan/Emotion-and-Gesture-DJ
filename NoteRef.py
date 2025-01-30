03190ce4ba56bf06876cd8420867ad42a75e1733
DanyurNan <92553506+DanyurNan@users.noreply.github.com>
2025-01-29 18:40:23 -0800

    small readability changes

--- NoteRef.py
+++ NoteRef.py
@@ -11,10 +11,11 @@ if __name__ == "__main__":
     synth = sound.Synth()
 
     key = "G major"
+    finger_map = 1
 
     base_note = KEY_OFFSET[key.split()[0]]
     scale = key.split()[1]
-    mapping = SCALE_MAPPINGS[scale][1] #Mapping changes based on some user control (swipe gesture or from interface)
+    mapping = SCALE_MAPPINGS[scale][finger_map] #Mapping changes based on some user control (swipe gesture or from interface)
 
     for i in range(5):  #i correspondes to finger press received from client
         threading.Thread(target=synth.play_note, args=(base_note + mapping[i],)).start()
