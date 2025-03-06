d1af8d2a68224dc1699ee0885fc2d1c689f08e49
DanyurNan <92553506+DanyurNan@users.noreply.github.com>
2025-03-05 16:30:13 -0800

    added swipe gestures

--- client.py
+++ client.py
@@ -4,9 +4,16 @@ import keyboard
 import sound
 import threading
 
-ESP32_IP = '192.168.217.79' 
+ESP32_IP = '192.168.26.79' 
 PORT = 10000
-NOTE_PRESSES = [60, 62, 64, 65, 67, 69]
+#NOTE_PRESSES = [60, 62, 64, 65, 67, 69]
+KEY_OFFSET = {'C':48, 'D': 50, 'E': 52, 'F':53, 'G':55, 'A': 57, 'B': 59}
+SCALE_MAPPINGS = {'major': [[0,2,4,5,7], [0,4,7,11,12], [5,7,9,10,12]], 
+                  'minor': [[0,2,3,5,7], [0,3,7,8,12], [5,7,8,10,12]]}
+
+base_note = KEY_OFFSET['C']
+scale = 'major'
+mapping = SCALE_MAPPINGS[scale][0] 
 
 def run_client(): #CALLED WHEN GUI IS CALLED, SHOULD RUN IN THE BACKGROUND AS THREAD
     synth = sound.Synth()
@@ -26,13 +33,16 @@ def run_client(): #CALLED WHEN GUI IS CALLED, SHOULD RUN IN THE BACKGROUND AS TH
             message = data.decode().strip()
             if message.split()[0] == "press":
                 print("Note press on sensor", message.split()[1])
-                threading.Thread(target=synth.play_note, args=(NOTE_PRESSES[int(message.split()[1])],)).start() #SHOULD BE SENT TO METHOD IN GUI INSTEAD, GUI CHOOSES NOTE TO PLAY
+                threading.Thread(target=synth.play_note, args=(base_note + mapping[int(message.split()[1])],)).start() #SHOULD BE SENT TO METHOD IN GUI INSTEAD, GUI CHOOSES NOTE TO PLAY
             elif message == "swipe left":
                 print("Hand swipe left") 
-                #GUI method
+                mapping = SCALE_MAPPINGS[scale][0] 
             elif message == "swipe right":
                 print("Hand swipe right") 
-                #GUI method
+                mapping = SCALE_MAPPINGS[scale][2] 
+            elif message == "swipe forward":
+                print("Hand swipe forward") 
+                mapping = SCALE_MAPPINGS[scale][1] 
             elif message.split()[0] == "disconnected":
                 print("Sensor", message.split()[1], "is disconnected, check wiring and reset.")
     except KeyboardInterrupt:
