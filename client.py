d0b4f731f7d1cba8b0c3cf2f9b8f4ccc6424039e
DanyurNan <92553506+DanyurNan@users.noreply.github.com>
2025-03-12 17:05:58 -0700

    stops client in gui

--- client.py
+++ client.py
@@ -19,7 +19,13 @@ SCALE_MAPPINGS = {'major': [[0,2,4,5,7], [0,4,7,11,12], [5,7,9,11,12]],
 # Test Intialization of Key and Scale
 base_note = KEY_OFFSET['D']
 scale = 'major'
-mapping = SCALE_MAPPINGS[scale][0] 
+mapping = SCALE_MAPPINGS[scale][0]
+
+running = True
+
+def change_key(gen_key, gen_scale):
+    base_note = KEY_OFFSET[gen_key]
+    scale = gen_scale
 
 def run_client():
     synth = sound.Synth() # Initialize Soundfont Script
@@ -31,43 +37,46 @@ def run_client():
     print("Connected to ESP32 server")
 
     # Check for ESP32 Messages
-    try:
-        while True:
+    while running:
 
-            # Reset Sensors if Disconnected
-            if keyboard.is_pressed('space'):
-                data = "r"
-                print("reseting sensors")
-                client_socket.send(data.encode())
-            data = client_socket.recv(1024)
-            if not data:
-                break
+        # Reset Sensors if Disconnected
+        if keyboard.is_pressed('space'):
+            data = "r"
+            print("reseting sensors")
+            client_socket.send(data.encode())
+        data = client_socket.recv(1024)
+        if not data:
+            break
 
-            message = data.decode().strip()
+        message = data.decode().strip()
 
-            # Check for Finger Movement
-            if message.split()[0] == "press":
-                print("Note press on sensor", message.split()[1] + ", Note", base_note + mapping[int(message.split()[1])]) # Converts Finger Number to Pitch using Mapping
-                threading.Thread(target=synth.play_note, args=(base_note + mapping[int(message.split()[1])],)).start()
+        # Check for Finger Movement
+        if message.split()[0] == "press":
+            print("Note press on sensor", message.split()[1] + ", Note", base_note + mapping[int(message.split()[1])]) # Converts Finger Number to Pitch using Mapping
+            threading.Thread(target=synth.play_note, args=(base_note + mapping[int(message.split()[1])],)).start()
 
-            # Check for Hand Swipes to Change Note Mapping
-            elif message == "swipe left":
-                print("Hand swipe left") 
-                mapping = SCALE_MAPPINGS[scale][0] 
-            elif message == "swipe right":
-                print("Hand swipe right") 
-                mapping = SCALE_MAPPINGS[scale][2] 
-            elif message == "swipe forward":
-                print("Hand swipe forward") 
-                mapping = SCALE_MAPPINGS[scale][1] 
+        # Check for Hand Swipes to Change Note Mapping
+        elif message == "swipe left":
+            print("Hand swipe left") 
+            mapping = SCALE_MAPPINGS[scale][0] 
+        elif message == "swipe right":
+            print("Hand swipe right") 
+            mapping = SCALE_MAPPINGS[scale][2] 
+        elif message == "swipe forward":
+            print("Hand swipe forward") 
+            mapping = SCALE_MAPPINGS[scale][1] 
 
-            # Displays Message if Sensor is Disconnected
-            elif message.split()[0] == "disconnected":
-                print("Sensor", message.split()[1], "is disconnected, check wiring and reset.")
+        # Displays Message if Sensor is Disconnected
+        elif message.split()[0] == "disconnected":
+            print("Sensor", message.split()[1], "is disconnected, check wiring and reset.")
 
-    except KeyboardInterrupt:
-        print("Connection closed by client")
+    print("Connection closed by client")
     client_socket.close()
+
+def close_client():
+    global running
+    running = False
+
 
 if __name__ == "__main__":
     run_client()
